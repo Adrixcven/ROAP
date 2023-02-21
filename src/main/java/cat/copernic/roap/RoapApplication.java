c4694b76d24386974a64242b1f4f751c44762683
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2023-02-21 08:16:10 +0100

    Pujada Inicial

---
+++ src/main/java/cat/copernic/roap/RoapApplication.java
@@ -0,0 +1,13 @@
+package cat.copernic.roap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoapApplication.class, args);
+	}
+
+}
