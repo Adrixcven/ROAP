c3dd0ef696c0c3af445131397f286ff99edd161b
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2023-02-23 08:55:20 +0100

    Arreglos

--- src/main/java/cat/copernic/roap/Encargos/controladores/ControladorGestionarPrendas.java
+++ src/main/java/cat/copernic/roap/Encargos/controladores/ControladorGestionarPrendas.java
@@ -5,6 +5,9 @@
 package cat.copernic.roap.Encargos.controladores;
 
 import cat.copernic.roap.Pedidos.controladores.*;
+import cat.copernic.roap.entidades.Pedidos;
+import cat.copernic.roap.entidades.Prenda;
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,6 +19,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ControladorGestionarPrendas {
     @GetMapping("/gestionarPrendas")
     public String inici(){ //Aquest és el mètode que generarà la resposta (recurs a retornar)
+        var prenda = new Prenda();
+        prenda.setNombre ("Juan");
+        prenda.setUnidades (3);
+        prenda.setPrecio (20);
+        prenda.setPrendaRopa ("Pantalon cargo");
+        prenda.setEstadoProducto ("OK");
+        prenda.setTalla (4);
+        prenda.setColor("Negro");
+        var prenda1 = new Prenda();
+        prenda.setNombre ("Alba");
+        prenda.setUnidades (7);
+        prenda.setPrecio (10);
+        prenda.setPrendaRopa ("Camiseta frozen");
+        prenda.setEstadoProducto ("OK");
+        prenda.setTalla (20);
+        prenda.setColor("Azul");
+        
+        var prendas = new ArrayList<Prenda>();
+        prendas.add(prenda);
+        prendas.add(prenda1);
         //log.info("Executant el controlador Spring MVC"); //Afegeix al log el missatge passat com a paràmetre.
         return "Encargos/GestionarPrendas"; //Retorn de la pàgina Login.html.
     }
--- src/main/java/cat/copernic/roap/Encargos/controladores/ControladorModificarPrenda.java
+++ src/main/java/cat/copernic/roap/Encargos/controladores/ControladorModificarPrenda.java
@@ -5,6 +5,8 @@
 package cat.copernic.roap.Encargos.controladores;
 
 import cat.copernic.roap.Pedidos.controladores.*;
+import cat.copernic.roap.entidades.Prenda;
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,6 +18,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ControladorModificarPrenda {
     @GetMapping("/modificarPrendas")
     public String inici(){ //Aquest és el mètode que generarà la resposta (recurs a retornar)
+        var prenda = new Prenda();
+        prenda.setNombre ("Juan");
+        prenda.setUnidades (3);
+        prenda.setPrecio (20);
+        prenda.setPrendaRopa ("Pantalon cargo");
+        prenda.setEstadoProducto ("OK");
+        prenda.setTalla (4);
+        prenda.setColor("Negro");
+        var prenda1 = new Prenda();
+        prenda.setNombre ("Alba");
+        prenda.setUnidades (7);
+        prenda.setPrecio (10);
+        prenda.setPrendaRopa ("Camiseta frozen");
+        prenda.setEstadoProducto ("OK");
+        prenda.setTalla (20);
+        prenda.setColor("Azul");
+        
+        var prendas = new ArrayList<Prenda>();
+        prendas.add(prenda);
+        prendas.add(prenda1);
         //log.info("Executant el controlador Spring MVC"); //Afegeix al log el missatge passat com a paràmetre.
         return "Encargos/ModificarPrenda"; //Retorn de la pàgina Login.html.
     }
