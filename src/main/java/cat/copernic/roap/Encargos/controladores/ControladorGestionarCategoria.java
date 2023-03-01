c5a4e8cd9d8a5e316e244b66237af775131be698
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2023-03-01 13:01:23 +0100

    Tasca 6A: modul encargos, pujada html amb bbdd i modificació modelos i controllers de Categoria

--- src/main/java/cat/copernic/roap/Encargos/controladores/ControladorGestionarCategoria.java
+++ src/main/java/cat/copernic/roap/Encargos/controladores/ControladorGestionarCategoria.java
@@ -4,8 +4,11 @@
  */
 package cat.copernic.roap.Encargos.controladores;
 
+import cat.copernic.roap.DAO.CategoriasDAO;
 import cat.copernic.roap.Pedidos.controladores.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -14,8 +17,11 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class ControladorGestionarCategoria {
+    @Autowired //Anotació que injecta tots els mètodes i possibles dependències de GosDAO al controlador
+    private CategoriasDAO CategoriasDAO;
     @GetMapping("/gestionarCategoria")
-    public String inici(){ //Aquest és el mètode que generarà la resposta (recurs a retornar)
+    public String inici(Model model) { //Aquest és el mètode que generarà la resposta (recurs a retornar)
+        model.addAttribute("Categorias", CategoriasDAO.findAll());
         //log.info("Executant el controlador Spring MVC"); //Afegeix al log el missatge passat com a paràmetre.
         return "Encargos/GestionarCategoria"; //Retorn de la pàgina Login.html.
     }
