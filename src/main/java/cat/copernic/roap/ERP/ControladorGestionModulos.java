cbeaacc7d85a7562d8a54cf24f640dffce892306
Adrià Ferrer Sanz <ferrer.sanz.adria@alumnat.copernic.cat>
2023-03-01 16:34:07 +0100

    Tarea 6A. Arreglos de HTML y Controladores
    No funcionan ni el gestor de modulos ni los selector.

--- src/main/java/cat/copernic/roap/ERP/ControladorGestionModulos.java
+++ src/main/java/cat/copernic/roap/ERP/ControladorGestionModulos.java
@@ -4,7 +4,10 @@
  */
 package cat.copernic.roap.ERP;
 
+import cat.copernic.roap.DAO.ModulosDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -13,8 +16,17 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class ControladorGestionModulos {
+    @Autowired //Anotació que injecta tots els mètodes i possibles dependències de GosDAO al controlador
+    private ModulosDAO ModulosDAO;
     @GetMapping("/gestionmodulo")
-    public String inici(){ //Aquest és el mètode que generarà la resposta (recurs a retornar)
+    public String inici(Model model){ //Aquest és el mètode que generarà la resposta (recurs a retornar)
+        
+        var pedidos = ModulosDAO.findById(1);
+        var inventario = ModulosDAO.findById(2);
+        var encargos = ModulosDAO.findById(3);
+        model.addAttribute("pedidos", pedidos );
+        model.addAttribute("inventario", inventario );
+        model.addAttribute("encargos", encargos );
         //log.info("Executant el controlador Spring MVC"); //Afegeix al log el missatge passat com a paràmetre.
         return "GestionModulos"; //Retorn de la pàgina Login.html.
     }
