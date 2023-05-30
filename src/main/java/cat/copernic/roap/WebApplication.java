25474265f5ce2415c000b4d60c9f74d00b83ab07
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2023-05-30 17:07:23 +0200

    Pujada validacions Marc

--- src/main/java/cat/copernic/roap/WebApplication.java
+++ src/main/java/cat/copernic/roap/WebApplication.java
@@ -64,7 +64,7 @@ public class WebApplication {
         return http.csrf().disable().authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/pedidos", "/pedidos/**").hasAnyAuthority("1", "3")
                 .requestMatchers("/encargos", "/gestionusers", "/gestionmodulo").hasAuthority("1")
-                .requestMatchers("/encargosProveedor").hasAuthority("2")
+                .requestMatchers("/encargosproveedor").hasAuthority("2")
                 .requestMatchers("/remember", "/gestionusers/adduser", "/guardarUsuario", "/**").permitAll()
                 .anyRequest().authenticated() 
                 )
